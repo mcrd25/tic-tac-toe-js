596e3b1e246edf9d1fb91b86f940ce0ac7b30f0f
Adriaan Beiertz <adriaanbd@gmail.com>
2019-08-09 17:11:50 -0500

    board module with grid, get cell and set cell

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,32 @@
-'use strict';
-import { playerFactory } from './components/player.js';
+import { playerFactory } from './components/player';
+//const player = require('./components/player');
 
-let player = playerFactory('maya', 'X');
 
-console.log(player);
+// PLAYER 
+const playerFactory = (name, symbol) => {
+	return { name, symbol }
+}
+
+// BOARD
+const board = (() => {
+	let  grid = [
+		[null, null, null],
+		[null, null, null], 
+		[null, null, null]
+	];
+
+	const getCell = (row, cell) => {
+		return grid[row][cell]
+	}
+
+	const setCell = (row, col, value) => {
+		grid[row, col] = value;
+	}
+})();
+
+// GAME
+
+let player_x = player.playerFactory('maya', 'X');
+let player_o = player.playerFactory('john', 'X');
+
+console.log(player_x);
