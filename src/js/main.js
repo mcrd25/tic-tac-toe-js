2aaa06d090031543050453192d1c6d33669e258c
Adriaan Beiertz <adriaanbd@gmail.com>
2019-08-16 15:31:14 -0500

    render grid

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,37 @@ const oSym = 'O';
 let playerX = playerFactory('maya', xSym);
 let playerO = playerFactory('john', oSym);
 
-const render = () => {
+const renderGrid = (container) => {
+	createGrid(container, 8);
+	
+}
 
+const createGrid = (container, cells) => {
+	const root = document.querySelector(container);
+	const grid = document.createElement('div')
+	grid.className = 'row'
+	for (let index = 0; index <= cells; index++) {
+		let cell = document.createElement('div');
+		// could add event listener her to each cell
+		cell.id = index
+		if ([2, 5, 8].includes(index)) {
+			cell.className = "cell col s4 last"
+		} else if ([0, 3, 6].includes(index)) {
+			cell.className = "cell col s4 first"
+		} else {
+			cell.className = "cell col s4"
+		}
+		grid.appendChild(cell)
+	}
+	root.appendChild(grid);
 }
 
 const getSymbol = (symbol) => {
 	if (symbol === 'O') return '<i class="material-icons symbol">radio_button_unchecked</i>';
-	if (symbol === 'X') return '<i class="material-icons symbol">close</i>';
+	if (symbol === 'X') return '<i class="material-icons symbol">clear</i>';
 }
+
+renderGrid('.container');
 
 const cells = document.querySelectorAll('.cell');
 game.setPlayers([playerX, playerO]);
