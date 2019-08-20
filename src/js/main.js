c008090ad5ba2afb16f5e7c514d94fbf3085cdf9
mcrd25 <mayacrd25@gmail.com>
2019-08-20 18:49:31 -0400

    Fixed grid layout bug and made gameplay work with new layout

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,34 @@ import { playerFactory } from './components/player.js';
 import { board } from './components/board.js';
 import { game } from './components/game.js';
 
-// GAME
+// GAME 
 const xSym = 'X';
 const oSym = 'O';
-const playerX = playerFactory('maya', xSym);
-const playerO = playerFactory('adriaan', oSym);
 
+const setPlayers = (players) => {
+	const [playerXName, playerOName] = players;
+	const playerX = playerFactory(playerXName, xSym);
+	const playerO = playerFactory(playerOName, oSym);
+	game.setPlayers([playerX, playerO]);
+}
+const getSymbol = (symbol) => {
+	if (symbol === 'O') return '<i class="material-icons symbol">radio_button_unchecked</i>';
+	if (symbol === 'X') return '<i class="material-icons symbol">clear</i>';
+};
 
+const play = (players) => {
+	const cells = document.querySelectorAll('.cell');
+	setPlayers(players);
+	game.setBoard(board);
+	changeCells(cells);
+};
+
+// DOM
 const render = (container) => {
 	const root = document.querySelector(container);
 	startScreen(root);
 	getNames(root);
 }
-
 const createGridDOM = () => {
 	const root = document.querySelector('.container')
 	const cells = 8;
@@ -23,10 +38,10 @@ const createGridDOM = () => {
 		let cell = document.createElement('div');
 		// could add event listener her to each cell
 		cell.id = index
-		cell.className = "cell col s4"
+		cell.className = 'cell col s4'
 		grid.appendChild(cell)
 	}
-	root.setAttribute("style", "padding-top: 4rem;")
+	root.setAttribute('style', 'padding-top: 4rem;')
 	root.appendChild(grid);
 }
 
@@ -35,20 +50,20 @@ const drawGrid = () => {
 	const form = document.querySelector('form');
 	const cells = document.querySelectorAll('.cell');
 	const start = document.querySelector('#start')
-	row.classList.remove("teal", "card-panel", "z-depth-2");
+	row.classList.remove('teal', 'card-panel', 'z-depth-2', 'player-wrapper');
 	row.removeChild(form);
 	row.removeChild(start);
 
 	// draw border bottom
 	for (let index = 0; index < cells.length - 3; index++) {
-		cells[index].setAttribute("style", "border-bottom: 8px solid black;")
+		cells[index].setAttribute('style', 'border-bottom: 8px solid black;')
 	}
 
 	// draw other borders
-	[0, 3].forEach(i => cells[i].classList.add("first"));
-	[2, 5].forEach(i => cells[i].classList.add("last"));
-	cells[6].classList.add("first");
-	cells[8].classList.add("last");
+	[0, 3].forEach(i => cells[i].classList.add('first'));
+	[2, 5].forEach(i => cells[i].classList.add('last'));
+	cells[6].classList.add('first');
+	cells[8].classList.add('last');
 }
 
 
@@ -57,13 +72,13 @@ const startScreen = (root) => {
 	const row = document.createElement('div')
 	row.classList.add('row');
 	row.id = 'grid';
-	row.classList.add("card-panel");
-	row.classList.add("teal", "lighten-4", "z-depth-2");
+	row.classList.add('card-panel');
+	row.classList.add('teal', 'lighten-4', 'z-depth-2');
 
-	row.style.height = "600px";
+	row.style.height = '600px';
 	const h2 = document.createElement('h2');
-	h2.innerHTML = "Welcome<br><br>Click Here to Start";
-	h2.id = "start";
+	h2.innerHTML = 'Welcome<br><br>Click Here to Start';
+	h2.id = 'start';
 
 	row.appendChild(h2);
 	root.appendChild(row);
@@ -73,13 +88,14 @@ const getNames = () => {
 	const mainRow = document.querySelector('#grid');
 	const form = document.createElement('form');
 	const subRow = document.createElement('div');
-	const input_x = document.createElement("div");
-	const input_o = document.createElement("div");
+	const input_x = document.createElement('div');
+	const input_o = document.createElement('div');
 	const title = document.querySelector('#start');
 	const input = document.createElement('input');
 
 	title.addEventListener('click', function() {
 		// remove black screen and text
+		mainRow.classList.add('player-wrapper');
 		form.className = 'col s12';
 		title.textContent = "Enter your names:";
 		subRow.className = 'row';
@@ -100,46 +116,33 @@ const getNames = () => {
 
 	})
 }
+
+const changeCells = (cells) => {
+	cells.forEach(cell => cell.addEventListener('click', function () {
+		if (game.getBoard().setCell(this.id, game.getCurrentPlayer().symbol)) {
+			this.innerHTML = getSymbol(game.getCurrentPlayer().symbol);
+			let gameOver = game.gameOver();
+			if (gameOver) {
+				if (gameOver === 'W') {
+					console.log(`The winner is ${game.getCurrentPlayer().name}`);
+				} else {
+					console.log(`Games is a draw`);
+				}
+				game.getBoard().reset();
+			}
+			if (!gameOver) game.switchPlayers();
+		}
+	}));
+};
 const getPlayers = (form) => {
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 		const players = [...form.elements].slice(0,2).map(elem => elem.value);
-		setPlayers(players);
 		createGridDOM()
 		drawGrid();
+		play(players);
 	});
 }
 
-const setPlayers = (players) => {
-	const [playerXName, playerOName] = players;
-	const playerX = playerFactory(playerXName, xSym);
-	const playerO = playerFactory(playerOName, oSym);
-	game.setPlayers([playerX, playerO]);
-	
-}
-const getSymbol = (symbol) => {
-	if (symbol === 'O') return '<i class="material-icons symbol">radio_button_unchecked</i>';
-	if (symbol === 'X') return '<i class="material-icons symbol">clear</i>';
-};
-
 render('.container');
 
-const cells = document.querySelectorAll('.cell');
-// game.setPlayers([playerX, playerO]);
-game.setBoard(board);
-
-cells.forEach(cell => cell.addEventListener('click', function() {
-	if (game.getBoard().setCell(this.id, game.getCurrentPlayer().symbol)) {
-		this.innerHTML = getSymbol(game.getCurrentPlayer().symbol);
-		let gameOver = game.gameOver();
-		if (gameOver) {
-			if (gameOver === 'W') {
-				console.log(`The winner is ${game.getCurrentPlayer().name}`);
-			} else {
-				console.log(`Games is a draw`);
-			}
-			game.getBoard().reset();
-		}
-		if (!gameOver) game.switchPlayers();
-	}
-}))
