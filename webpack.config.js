ace34121f3e79e858ae17221577ea8a99ae3a512
mcrd25 <mayacrd25@gmail.com>
2020-03-24 14:47:19 -0400

    Changed entry point of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/js/main.js',
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'dist'),
