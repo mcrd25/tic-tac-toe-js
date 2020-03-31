cf4ef7dfa8cd8a72d4452bb228bb4315edadb9be
mcrd25 <mayacrd25@gmail.com>
2020-03-31 16:32:52 -0400

    Added getPlayer and setPlayer tests and outline for remaining 5

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,1 +1,43 @@
-import game from '../src/js/components/game';
+import Game from '../src/js/components/game';
+
+
+describe('Game', () => {
+  const players = ['Maya', 'Adriaan'];
+  const players2 = ['player1', 'player2'];
+  describe('setPlayers(players)', () => {
+    test('should 2 set players for game', () => {
+      expect(Game.getPlayers()).toStrictEqual([undefined, undefined]);
+      Game.setPlayers(players);
+      expect(Game.getPlayers()).not.toStrictEqual([undefined, undefined]);
+      expect(Game.getPlayers()).toStrictEqual(players);
+    });
+  });
+  describe('getPlayers()', () => {
+    test('should get current players of game once set', () => {
+      Game.setPlayers(players2);
+      expect(Game.getPlayers()).toStrictEqual(players2);
+      Game.setPlayers(players);
+      expect(Game.getPlayers()).not.toStrictEqual(players2);
+      expect(Game.getPlayers()).toStrictEqual(players);
+    });
+  });
+  describe('setBoard(board)', () => {
+
+  });
+
+  describe('getBoard()', () => {
+
+  });
+
+  describe('getCurrentPlayer()', () => {
+
+  });
+
+  describe('switchPlayers()', () => {
+
+  });
+
+  describe('gameOver()', () => {
+
+  });
+});
